f72325629e66aa51714ef656ebef0455b3c3ed12
Padmini6699 <77808223+Padmini6699@users.noreply.github.com>
2022-01-28 15:01:29 -0600

    map filter reducing and sorting

--- 01mapper.py
+++ 01mapper.py
@@ -3,21 +3,21 @@
 
 f = open("purchases.txt","r")  # open file, read-only
 
-o = open("out01.txt", "w") # open file, write
+o = open("o.txt", "w") # open file, write
 
 for line in f:  
 
     rowList = line.strip().split("    ") 
 
-    print (rowList )
+    # print (rowList )
 
-    print (len(rowList ))
+    # print (len(rowList ))
 
     if len(rowList) == 6:
 
         date, time, location, dept, amount, payType = rowList  #assign names
 
-        print ("{0}\t{1}".format(location, amount))
+        # print ("{0}\t{1}".format(location, amount))
 
         o.write("{0}\t{1}\n".format(location, amount))
 
